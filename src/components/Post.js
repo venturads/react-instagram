f7802395199557fad1b2ee56cb1f7b09b5bf6ce5
venturads <albert@venturads.com>
2020-08-27 17:46:29 -0700

    added material, post css

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,22 @@
 import React from  'react';
+import Image1 from '../images/pug1.jpg'
+import '../styles/Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
     return (
-        <div>
-            <p>posts</p>
+        <div className="post">
+            <div className="post_header">
+                <Avatar
+                    className="post_avatar"
+                    alt='Venturads'
+                    src={Image1}
+                />
+                <h3 className="post_text">Username</h3>
+            </div>
+                <img className="post_image" src={Image1} />
+
+                <h4 className="post_text"><strong>username</strong>Username</h4>
         </div>
     )
 }
